95d97079b3bac0d9fc5ba8d9ee72d74b150c6ff8
xTarantino28 <gzanabriaz28@gmail.com>
2024-05-05 21:11:25 -0500

    arreglo de nombres de veth peers y namespaces

--- init_orchestrator/internal_net_headnode.sh
+++ init_orchestrator/internal_net_headnode.sh
@@ -7,6 +7,12 @@ DireccionRed="$3"   # formato CIDR
 RangoDHCP="$4"      # especificar formato
 brigde="br-int"
 
+echo $NombreRed
+echo $VLAN_ID
+echo $DireccionRed
+echo $RangoDHCP
+
+
 # Extraer la dirección de red y la máscara de subred
 direccion_red=$(echo $DireccionRed | cut -d '/' -f1)
 mascara_subred=$(echo $DireccionRed | cut -d '/' -f2)
