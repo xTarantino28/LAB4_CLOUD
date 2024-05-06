1133bfcf8a5d18a6eea7f0a0e22fce49925e172d
xTarantino28 <gzanabriaz28@gmail.com>
2024-05-06 00:27:04 -0500

    ejecucion de comandos en root con paramiko

--- implement_orchestrator/vm_script.sh
+++ implement_orchestrator/vm_script.sh
@@ -14,14 +14,7 @@ interfaz_tap_vm="$NombreOvS"-"$NombreVM"-tap
 ip tuntap add mode tap name "$interfaz_tap_vm"
 
 # crear VM (script lab2)
-qemu-system-x86_64 \
--enable-kvm \
--vnc 0.0.0.0:"$puertoVNC_local"\
--netdev tap,id="$interfaz_tap_vm",ifname="$interfaz_tap_vm",script=no,downscript=no \
--device e1000,netdev="$interfaz_tap_vm",mac=20:20:03:34:ee:0"$puertoVNC_local" \   # variar los dos ultimos
--daemonize \
--snapshot \
-cirros-0.5.1-x86_64-disk.img
+qemu-system-x86_64 -enable-kvm -vnc 0.0.0.0:"$puertoVNC_local" -netdev tap,id="$interfaz_tap_vm",ifname="$interfaz_tap_vm",script=no,downscript=no -device e1000,netdev="$interfaz_tap_vm",mac=20:20:03:34:ee:0"$puertoVNC_local" -daemonize -snapshot cirros-0.5.1-x86_64-disk.img
 
 # Conectar interfaz TAP al OvS del host local con el VLAN ID correspondiente
 ovs-vsctl add-port "$NombreOvS" "$interfaz_tap_vm" tag="$VLAN_ID"
